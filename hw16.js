ce52b54098e97584975f91171ac3f44e36908726
toshamamat <toshamamat@gmail.com>
2024-03-30 13:30:06 +0100

    hw16 2nd try

---
+++ hw16.js
@@ -0,0 +1,105 @@
+//ex1
+// let name = 'Tosha';
+// let student = name;
+// console.log(name, student);
+
+//ex2
+
+// let text = 'Hello';
+// let counter = 100500;
+// let isOpen = true;
+// let someValue = null; 
+
+// let typeText = typeof text;
+// let typeCounter = typeof counter;
+// let typeIsOpen = typeof isOpen;
+// let typeSomeValue = typeof someValue;
+
+// console.log('Hello', typeText)
+
+//ex4
+
+// const firstName = prompt('Enter your name');
+// const lastName = prompt('Enter your last name');
+// const fullName = firstName + ' ' + lastName;
+// alert(fullName);
+
+//ex5
+
+// let Pavel = 1500;
+// let Olga = 1200;
+// let Mark = 2100;
+// let sum = Pavel + Olga + Mark;
+// alert('total is' + ' ' + sum);
+
+
+// //block 2 ex1
+// let x = 20
+// let y = 58
+// let z = 42
+
+// let totalSum = x + y + z;
+// console.log(totalSum)
+
+//block2 ex2
+
+let secondsInMinute = 60;
+let minutesInHour = 60;
+let hoursInDay = 24;
+let daysInYear = 365;
+let myAge = 25;
+
+let myAgeInSeconds = secondsInMinute * minutesInHour * hoursInDay * daysInYear * myAge;
+console.log(myAgeInSeconds)
+
+//block2 ex3
+
+let count = 42;
+let userName = '42';
+const stringFromNumber = String(count); 
+const numberFromString = userName.toString();
+console.log(numberFromString);
+
+//block2 ex4
+
+// let a = '1';
+// let b = '2';
+// let c = 'белых медведей'
+
+// let result = a + b + c;
+// console.log(result);
+
+//block2 ex5
+let word1 = 'доступ';
+let word2 = 'наледь';
+let word3 = 'морпех';
+let word4 = 'попрек';
+let word5 = 'рубило';
+
+let lengthWords = word1.length +  word2.length + word3.length + word4.length + word5.length;
+console.log(lengthWords);
+
+//block2 ex6
+let number = 10;
+let string = 'Hello';
+let boolean = true;
+
+console.log(typeof string)
+
+//block2 ex7
+// const yourName = prompt('Enter your name');
+// const age = prompt('Enter your age');
+// console.log(yourName)
+// console.log(age);
+
+//block3 ex2
+
+// let codeWord1 = "обернись";
+// let codeWord2 = "неужели";
+// let codeWord3 = "огурцы";
+// let codeWord4 = "липкие";
+// let codeWord5 = "?!";
+
+// let cipher = codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5;
+// console.log(cipher)
+
