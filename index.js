0151e6a6ac55a75c2e3e6d0fa53aa0ecc698f0df
Lizaveta Yatsynovich <liza.yatsynovich@gmail.com>
2024-04-22 02:18:18 +0300

    Liza Buevich HW lesson 23

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const todos = document.querySelector('.todos');
+const todosItem = document.createElement('li');
+
+todosItem.classList.add('todos__item');
+todos.append(todosItem);
+
+const todosItemCheckbox = document.createElement('input');
+todosItemCheckbox.classList.add('todos__item-checkbox');
+todosItemCheckbox.setAttribute('type', 'checkbox');
+todosItemCheckbox.setAttribute('checked', '');
+todosItem.append(todosItemCheckbox);
+
+const todosItemText = document.createElement('p');
+todosItemText.classList.add('todos__item-text');
+todosItemText.textContent = 'Todo text';
+todosItem.append(todosItemText);
+
+const todosItemDelete = document.createElement('div');
+todosItemDelete.classList.add('todos__item-delete');
+todosItemDelete.innerHTML = `<button class="button">X</button> <span class="todos__item-date">Date</span>`;
+todosItem.append(todosItemDelete);
