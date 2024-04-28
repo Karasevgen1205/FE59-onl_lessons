f18dadd3f08d3612896361285e5777e3a64ed9ce
Kotofei4s <kotofot228@gmail.com>
2024-04-28 20:00:11 +0300

    second commite

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 "use strict";
+
+const addButton = document.querySelector('banner__top__module-buttonAdd');
+const list = document.querySelector('banner__bottom');
+const deleteAllButton = document.querySelector('banner__top__module-buttonDelAll');
+const deleteLastButton = document.querySelector('banner__bottom__block__container');
+const Date = document.querySelector('banner__bottom__block__container-textDate');
+
+    deleteAllButton.addEventListener('click', () => {
+    const todoList = document.querySelector('.banner__bottom');
+    todoList.innerHTML = '';
+    localStorage.clear();
+  });
+
+    deleteLastButton.addEventListener('click', () => {
+    const todoItems = document.querySelectorAll('.banner__bottom__block__container');
+    const lastItem = todoItems[todoItems.length - 1];
+    lastItem.remove();
+  });
+
+  function generateDate() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    return `${yyyy}-${mm}-${dd}`;
+  }
+  console.log(generateDate);
