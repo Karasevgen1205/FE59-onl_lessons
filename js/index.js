0af40b19894c9a0d2dd070459bb438894d0637a2
Kotofei4s <kotofot228@gmail.com>
2024-05-04 19:03:23 +0300

    fore commit

--- js/index.js
+++ js/index.js
@@ -8,29 +8,13 @@ const newDate = document.querySelector('.banner__bottom__block__container-textDa
 const todoItems = document.querySelector('.banner__bottom__block__container');
 const create = document.querySelector('.banner__top__module-input');
 const deleteButton = document.querySelectorAll('.banner__bottom__block__container-button');
+const checkboxes = document.querySelectorAll('banner_bottom__block__container-input');
 
-  const date = new Date().toISOString().slice(0, 10).split('-').reverse().join('.');
 
 
-    deleteAllButton.addEventListener('click', () => {
-    const todoList = list;
-    todoList.innerHTML = '';
-    });
+const date = new Date().toISOString().slice(0, 10).split('-').reverse().join('.');
 
-  deleteButton.forEach(item => {
-    item.addEventListener ('click', (event) => {
-    const currientBanner = event.target.parentElement;
-    list.removeChild (currientBanner);
-    });
-    
-  });
-
-    deleteLastButton.addEventListener('click', () => {
-    const lastItem = todoItems.parentElement.lastChild;
-    lastItem.remove();
-  });
-
-    addButton.addEventListener('click', () => {
+  addButton.addEventListener('click', () => {
     const inputText = create.value;
 
     if  (inputText) {
@@ -38,9 +22,9 @@ const deleteButton = document.querySelectorAll('.banner__bottom__block__containe
     
       todoAdd.classList.add('banner__bottom__block__container')
       todoAdd.innerHTML = `
-          <input class="banner_bottom__block__container-input" type="checkbox">
+            <input class="banner_bottom__block__container-input" type="checkbox" id="check">
             <p class="banner__bottom__block__container-text">${inputText}</p>
-            <button class="banner__bottom__block__container-button">x</button>
+            <button class="banner__bottom__block__container-button" onclick="deleteToDo(event)">x</button>
             <p class="banner__bottom__block__container-textDate">${date}</p>
         `;
       list.append(todoAdd);
@@ -48,9 +32,38 @@ const deleteButton = document.querySelectorAll('.banner__bottom__block__containe
 
       create.value = "";
     
-  });
+    });
 
+    deleteAllButton.addEventListener('click', () => {
+      const todoList = list;
+      todoList.innerHTML = '';
+    });
 
+    function deleteToDo(event) {
+      const curruentLi = event.target.parentElement;
+      list.removeChild(curruentLi);
+    }
 
+    deleteLastButton.addEventListener('click', () => {
+      const lastItem = list.lastChild;  
+      lastItem.remove();
+    })
+
+    function clickCheck(checkbox) {
+      const element = checkboxes.parentElement;
+      const textDatePass = element.querySelector('banner__bottom__block__container-text');
+      
+        checkbox.addEventListener('click', () => {
+          if (checkbox.checked) {
+            element.style.backgroundColor = 'gray'; 
+            textDatePass.style.textDecoration = 'line-through'; 
+          } else {
+            element.style.backgroundColor = '';
+            textDatePass.style.textDecoration = 'none';
+          }
+        });
+  }
+
+  checkboxes.forEach(clickCheck);
 
   
