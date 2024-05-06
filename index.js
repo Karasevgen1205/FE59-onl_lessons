9d655c71fff887a48936a4c458c1ca638a889973
Lizaveta Yatsynovich <liza.yatsynovich@gmail.com>
2024-05-07 00:04:19 +0300

    Liza Buevich HW lesson 28

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 "use strict";
+
+const timer = document.querySelector('.timer');
+const start = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+
+let seconds = 0;
+let interval;
+let isTimerRunning = 0;
+
+function updateTime() {
+    seconds++;
+    timer.textContent = `${seconds.toString().padStart(2, '0')} sec`;
+}
+
+start.addEventListener('click', () => {
+    if (!isTimerRunning) {
+        interval = setInterval(updateTime, 1000); 
+        isTimerRunning = true;
+    }
+})
+
+pause.addEventListener('click', () => {
+    if (isTimerRunning) {
+        clearInterval(interval);
+        isTimerRunning = false;
+    }
+});
+
